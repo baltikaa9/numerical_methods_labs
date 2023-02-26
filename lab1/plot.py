1b17d022d5b8c999f330176ce922e121d8a53535
baltikaa9 <sergey4016@mail.ru>
2023-02-27 01:53:53 +0300

    fixes

--- lab1/plot.py
+++ lab1/plot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 fig = plt.Figure(figsize=(5, 5), facecolor='ghostwhite')
 
+
 @dataclass
 class InputParameters:
     a: float
@@ -17,18 +18,21 @@ class InputParameters:
 def create_plot():
     ax_1 = fig.add_subplot(131)
     ax_1.set(title="f'(x)",
-            xlabel='x',
-            ylabel='y')
+             xlabel='x',
+             ylabel='y',
+             facecolor='ghostwhite')
     ax_1.grid()
     ax_2 = fig.add_subplot(132)
     ax_2.set(title="f''(x)",
              xlabel='x',
-             ylabel='y')
+             ylabel='y',
+             facecolor='ghostwhite')
     ax_2.grid()
     ax_3 = fig.add_subplot(133)
     ax_3.set(title="f'''(x)",
              xlabel='x',
-             ylabel='y')
+             ylabel='y',
+             facecolor='ghostwhite')
     ax_3.grid()
     return [ax_1, ax_2, ax_3]
 
