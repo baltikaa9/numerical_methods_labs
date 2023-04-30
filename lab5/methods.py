87fe7a70c2abda2c55810a096661b02deea35011
baltikaa9 <sergey4016@mail.ru>
2023-04-30 22:40:23 +0300

    f(x, y, z) -> system(x, y, z)

--- lab5/methods.py
+++ lab5/methods.py
@@ -18,17 +18,12 @@ class InputParameters:
         self.x = np.linspace(self.a, self.b, self.N)
 
 
-def f(num: Literal[0] | Literal[1], x: float, u: tuple[float, float]):
-    try:
-        match num:
-            case 0:
-                return cos(u[0] + 1.1 * u[1]) + 1
-            case 1:
-                return (1 / (x + 2.1 * u[0] ** 2)) + x + 1
-            case _:
-                raise IndexError(f'В системе только {len(u)} уравнений')
-    except ZeroDivisionError:
-        return
+def system(x: float, u: tuple[float, float]):
+    """
+    u[0] = y
+    u[1] = z
+    """
+    return cos(u[0] + 1.1 * u[1]) + 1, (1 / (x + 2.1 * u[0] ** 2)) + x + 1
 
 
 def runge_kutta_2(params: InputParameters):
@@ -44,8 +39,8 @@ def runge_kutta_2(params: InputParameters):
         z.append(u[1])
 
         # U_k+1
-        y0 += params.h * f(0, x + params.h / 2, (u[0] + (params.h / 2) * f(0, x, u), u[1]))
-        z0 += params.h * f(1, x + params.h / 2, (u[0], u[1] + (params.h / 2) * f(1, x, u)))
+        y0 += params.h * system(x + params.h / 2, (u[0] + (params.h / 2) * system(x, u)[0], u[1]))[0]
+        z0 += params.h * system(x + params.h / 2, (u[0], u[1] + (params.h / 2) * system(x, u)[1]))[1]
 
     return y, z
 
@@ -62,14 +57,13 @@ def runge_kutta_4(params: InputParameters):
         y.append(y0)
         z.append(z0)
 
-        k1 = f(0, x, u), f(1, x, u)
-        k2 = f(0, x + params.h / 2, (u[0] + k1[0] * params.h / 2, u[1])), \
-            f(1, x + params.h / 2, (u[0], u[1] + k1[1] * params.h / 2))
-        k3 = f(0, x + params.h / 2, (u[0] + k2[0] * params.h / 2, u[1])), \
-            f(1, x + params.h / 2, (u[0], u[1] + k2[1] * params.h / 2))
-        k4 = f(0, x + params.h, (u[0] + k3[0], u[1])), \
-            f(1, x + params.h, (u[0], u[1] + k3[1]))
-
+        k1 = system(x, u)[0], system(x, u)[1]
+        k2 = system(x + params.h / 2, (u[0] + k1[0] * params.h / 2, u[1]))[0], \
+            system(x + params.h / 2, (u[0], u[1] + k1[1] * params.h / 2))[1]
+        k3 = system(x + params.h / 2, (u[0] + k2[0] * params.h / 2, u[1]))[0], \
+            system(x + params.h / 2, (u[0], u[1] + k2[1] * params.h / 2))[1]
+        k4 = system(x + params.h, (u[0] + k3[0], u[1]))[0], \
+            system(x + params.h, (u[0], u[1] + k3[1]))[1]
 
         # U_k+1
         y0 += params.h * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]) / 6
